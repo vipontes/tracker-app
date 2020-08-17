88818d9a18fad35669ba5459aeb73d54e735914e
vipontes <vipontes70@gmail.com>
2020-08-17 10:49:43 -0300

    Correcting errors

--- app/src/main/java/br/net/easify/tracker/di/module/RepositoryModule.kt
+++ app/src/main/java/br/net/easify/tracker/di/module/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package br.net.easify.tracker.di.module
 
 import android.app.Application
+import br.net.easify.tracker.repositories.RoutePathRepository
+import br.net.easify.tracker.repositories.RouteRepository
 import br.net.easify.tracker.repositories.TokenRepository
 import br.net.easify.tracker.repositories.UserRepository
 import dagger.Module
@@ -16,10 +18,21 @@ class RepositoryModule {
         return UserRepository(application)
     }
 
-
     @Provides
     @Singleton
     fun provideTokenRepository(application: Application): TokenRepository {
         return TokenRepository(application)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRouteRepository(application: Application): RouteRepository {
+        return RouteRepository(application)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoutePathRepository(application: Application): RoutePathRepository {
+        return RoutePathRepository(application)
     }
 }
